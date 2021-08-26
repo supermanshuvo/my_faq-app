0e2f8933076960eaf3f3ef7ae6d5fad3cd958ec8
supermanshuvo <shuvothesuperman@gmail.com>
2021-08-27 02:07:51 +0600

    Create function for add btn

--- js/myjs.js
+++ js/myjs.js
@@ -5,3 +5,9 @@ let faq_input_data      = document.querySelector('#add_to_faq_list'),
     faq_list_answer     = document.getElementById('answer'),
     faq_add_btn         = document.getElementById('create');
 const url = "https://jsonplaceholder.typicode.com/posts";
+
+// Add Data to server
+faq_add_btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    add_faq();
+});
