e7d1e41883c62ddff11485e8d55a0e404453a662
supermanshuvo <shuvothesuperman@gmail.com>
2021-08-27 01:52:47 +0600

    Change the header size of form and de-clear variable

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,10 @@
 // De-clearing Variable
+let faq_input_data = document.querySelector('#add_to_faq_list'),
+faq_list_item = document.querySelector('#faq-list'),
+faq_list_question = document.getElementById('question'),
+faq_list_answer = document.getElementById('answer');
+
+function show(){
+    
+}
+faq_input_data.addEventListener('submit',show);
