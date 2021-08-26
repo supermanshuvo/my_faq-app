dd89fd2c8eedd249d0027c099fc3d32ff621ab5f
supermanshuvo <shuvothesuperman@gmail.com>
2021-08-27 02:05:02 +0600

    Create url link and save it

--- js/myjs.js
+++ js/myjs.js
@@ -1,10 +1,7 @@
 // De-clearing Variable
-let faq_input_data = document.querySelector('#add_to_faq_list'),
-faq_list_item = document.querySelector('#faq-list'),
-faq_list_question = document.getElementById('question'),
-faq_list_answer = document.getElementById('answer');
-
-function show(){
-    
-}
-faq_input_data.addEventListener('submit',show);
+let faq_input_data      = document.querySelector('#add_to_faq_list'),
+    faq_list_item       = document.querySelector('#faq-list'),
+    faq_list_question   = document.getElementById('question'),
+    faq_list_answer     = document.getElementById('answer'),
+    faq_add_btn         = document.getElementById('create');
+const url = "https://jsonplaceholder.typicode.com/posts";
