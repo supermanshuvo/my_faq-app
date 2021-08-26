4e26b7ee3929435a8d1116c99d800f77138490af
supermanshuvo <shuvothesuperman@gmail.com>
2021-08-27 04:25:47 +0600

    Delete function is done

--- js/myjs.js
+++ js/myjs.js
@@ -6,8 +6,7 @@ let faq_input_data      = document.querySelector('#add_to_faq_list'),
     faq_list_answer     = document.getElementById('answer'),
     faq_add_btn         = document.getElementById('create'),
     faq_success_sms     = document.getElementById('success_sms'),
-    item = '',
-    faqCustomId =101;
+    item = '';
 const url = "https://jsonplaceholder.typicode.com/posts";
 
 // Add Data to server
@@ -19,7 +18,7 @@ function add_faq(){
     fetch(url,{
         method:'POST',
         body: JSON.stringify({
-            id : faqCustomId,
+            id : 404,
             title: faq_list_question.value,
             body: faq_list_answer.value,
         }),
@@ -33,9 +32,9 @@ function add_faq(){
         data.push(json);
         show_faq(data);
         faq_success_sms.innerHTML=`<div class="alert alert-success alert-dismissible fade show" role="alert">
-     FAQ Successfully Added!
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>`;
+                                    FAQ Successfully Added!
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>`;
     });
     faq_list_answer.value = '';
     faq_list_question.value = '';
@@ -45,10 +44,11 @@ function add_faq(){
 function show_faq(data){
     data.forEach(data => {
         item+=`<tr>
-        <td>${data.id}</td>
-        <td>${data.title}</td>
-        <td>${data.body}</td>
-    </tr>`;
+                <td>${data.id}</td>
+                <td>${data.title}</td>
+                <td>${data.body}</td>
+                <td><button class="btn btn-warning btn-sm" onclick="deleteFAQ(${data.id})">Delete</button></td>
+            </tr>`;
     });
     faq_list_item.innerHTML=item;
 }
@@ -58,8 +58,29 @@ document.addEventListener('DOMContentLoaded',()=>{
     .then((posts)=>show_faq(posts))
     .catch((err)=>{
         faq_success_sms.innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
-     FAQ found ${err}!
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div>`;
+                                        FAQ found ${err}!
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>`;
     })
 });
+
+function deleteFAQ(dataId){
+    let deleteBtn = event.target;
+    let item = deleteBtn.parentElement;
+    let id = item.dataset.id;
+    let ask = confirm('Do you want to delete the Item');
+    if(ask){
+        let itemParent = item.parentElement;
+        let final = itemParent.parentElement;
+        fetch(`${url}/${dataId}`,{
+            method: 'DELETE',
+        })
+        .then(()=>{
+            final.removeChild(itemParent);
+            faq_success_sms.innerHTML=`<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                        FAQ Delete done SL number: ${dataId} !
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>`;
+        })
+    }
+}
