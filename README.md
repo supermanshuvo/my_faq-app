336f70e950e0242030478fe4919efa85f4bae045
Rezaul Karim <shuvothesuperman@gmail.com>
2021-09-27 19:07:33 +0600

    update the work

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# my_faq-app
-This web app only use for store your FAQ
+# My FAQ App
+### Using Details
+First you need to clone this app
+`git clone https://github.com/supermanshuvo/my_faq-app.git`
+Then you need to run
+`npm install`
+This will install all the dependencies that you need
+
+After installing this
+you need to run the fake api 
+`npm run json:server`
+
+After all of this you need to run the `index.html` or open with live server
+
+I am using rest api for the project. I need to update the style.
+I have a problem with `e.preventDefault();`
+Need some suggestion about the project.
+Kindly `Help me!`.
+That will be good for my learning.
+by `Superman Shuvo`
---
+++ public/app.js
@@ -0,0 +1,123 @@
+"use strict"
+let question = document.querySelector('#question'),
+    answer = document.querySelector('#answer'),
+    save = document.querySelector('#save'),
+    faq_list_item = document.querySelector('#faq-list'),
+    update_btn = document.getElementById('update'),
+    item = '';
+
+const url = "http://localhost:3000/faqs";
+
+// Show Faq Function
+document.addEventListener('DOMContentLoaded',()=>{
+    fetch(url)
+    .then((response)=>response.json())
+    .then((faqs)=>show_faq(faqs))
+    .catch((err)=>{
+        console.log(`Fetch `+err);
+    });
+    question.value='';
+    answer.value ='';
+})
+function show_faq(data){
+    data.forEach(data=>{
+        item+=`<tr>
+        <td class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">${data.id}</td>
+        <td id="questionTd" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">${data.question}</td>
+        <td id="answerTd" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">${data.answer}</td>
+        <td class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"><button onclick="editFAQ(${data.id})">Edit</button> <button onclick="deleteFAQ(${data.id})">Delete</button></td>
+        </tr>`
+    });
+    faq_list_item.innerHTML = item;
+}
+
+// Add Faq in list
+save.addEventListener('click',function(e){
+    e.preventDefault();
+    if(question.value.length==0 && answer.value.length==0){
+        alert('Kindly Enter Something');
+    }else{
+        fetch(url,{
+            method:'POST',
+            body:JSON.stringify({
+                question:question.value,
+                answer:answer.value
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            let data = [];
+            data.push(json);
+            show_faq(data);
+            console.log('Add Success');
+        });
+    }
+})
+
+// Delete Faq Function 
+function deleteFAQ(datId){
+    let deleteBtn = event.target;
+    let item = deleteBtn.parentElement;
+    let ask = confirm('Do you want to delete the Item');
+    if(ask){
+        let itemParent = item.parentElement;
+        let final_item = itemParent.parentElement;
+        fetch(`${url}/${datId}`,{
+            method:'DELETE',
+        })
+        .then(()=>{
+            final_item.removeChild(itemParent);
+            console.log('Delete success');
+        })
+    }
+};
+
+// Edit Faq function
+function editFAQ(dataId){
+    save.style.display = "none";
+    update_btn.style.display = "block";
+    let hiddenId = document.getElementById('hiddenId');
+    hiddenId.value = dataId;
+    fetch(url)
+    .then((response)=>response.json())
+    .then((items)=>{
+        items.map((item)=>{
+            if(parseInt(hiddenId.value)===parseInt(item.id)){
+                question.value = item.question;
+                answer.value = item.answer;
+            }
+        });
+    })
+    .catch((err)=>console.log(err))
+};
+
+// let update_btn = document.getElementById('update');
+update_btn.addEventListener('click',function(e){
+    e.preventDefault();
+    if(question.value.length==0 && answer.value.length==0){
+        alert('Kindly Enter Something');
+    }else{
+        fetch(`${url}/${hiddenId.value}`,{
+            method: 'PUT',
+            body:JSON.stringify({
+                question:question.value,
+                answer:answer.value
+            }),
+            headers:{
+                'Content-type':'application/json; charset=UTF-8',
+            },
+        })
+        .then((response)=>response.json())
+        .then((item)=>{
+            console.log(item[hiddenId.value]);
+        })
+        .catch((err)=>console.log(err))
+    }
+    update_btn.style.display="none";
+    save.style.display = "block";
+    question.value='';
+    answer.value = '';
+})
